7f6f93fa6e01496be34599bcebb435af2c81fcc3
Shashikant <116100338+ShashikantK7@users.noreply.github.com>
2023-02-11 00:00:22 +0530

    Update find_pi.sh

--- find_pi.sh
+++ find_pi.sh
@@ -1,1 +1,16 @@
+#!/bin/bash/
+
+echo 'clone the repo to  given system ..'
+git clone https://github.com/ros/ros_tutorials.git
+
+echo 'listing content'
+la -la
+
+echo 'go back to home folder'
+cd
+
+echo 'now put the command to find pi'
+grep -r "3.14159" /home/ros_tutorials/
+
+echo 'done'
 
